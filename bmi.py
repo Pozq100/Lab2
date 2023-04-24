b51dec75b315d13f42658033e0cc4ddf151c4516
Pozq100 <chenjc.22@ichat.sp.edu.sg>
2023-04-24 09:08:04 +0800

    Hello

---
+++ bmi.py
@@ -0,0 +1,6 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight/(height * height)
+    print(bmi)
+calculate_bmi(height = 1.73,weight = 57)
