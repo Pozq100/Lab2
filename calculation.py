ce524d295220f7da171f276f6bcba3ecff427e7e
Pozq100 <chenjc.22@ichat.sp.edu.sg>
2023-05-08 08:35:19 +0800

    hey

--- calculation.py
+++ calculation.py
@@ -5,6 +5,7 @@ def get_userinput():
     for i in range(len(num_list)):
         num_list[i] = int(num_list[i])
     return num_list
+
 def calculate_average(num_list):
     length = len(num_list)
     total = 0
@@ -13,11 +14,14 @@ def calculate_average(num_list):
     print(f"Average: ", total/length)
     return
 
+
 def calculate_min_max(num_list):
     print(f"Min,Max:", num_list[0], num_list[len(num_list) - 1])
     return
+
 def sort_numlist(num_list):
     return sorted(num_list)
+
 def calculate_median(num_list):
     length = len(num_list)
     if length % 2 == 0:
@@ -29,6 +33,7 @@ def calculate_median(num_list):
         median = num_list[mid]
     print(f"Median: ", median)
     return
+
 def main():
     num_list = get_userinput()
     num_list = sort_numlist(num_list)
