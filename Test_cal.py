306d64af0f8745c7e724c2f5472d66accdfd9dcc
Pozq100 <chenjc.22@ichat.sp.edu.sg>
2023-05-14 16:59:16 +0800

    Lab2 Test

--- Test_cal.py
+++ Test_cal.py
@@ -1,8 +1,27 @@
-import unittest
+import calculation
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-if __name__ == '__main__':
-    unittest.main()
+def test_average():
+    num_list = [0, 98, 123, 23, 56]
+    average = calculation.calculate_average(num_list)
+    answer = 60
+    assert(average == answer)
+
+def test_minmax():
+    num_list = [0, 98, 123, 23, 56]
+    min_max = calculation.calculate_min_max(num_list)
+    answer = [0,123]
+    assert(min_max == answer)
+
+def test_median_odd():
+    num_list = [0, 98, 123, 23, 56]
+    median = calculation.calculate_median(num_list)
+    answer = 56
+    assert(answer == median)
+
+def test_median_even():
+    num_list = [0, 98, 123, 23, 56, 68]
+    median = calculation.calculate_median(num_list)
+    answer = 62
+    assert(answer == median)
+
--- calculation.py
+++ calculation.py
@@ -11,35 +11,37 @@ def calculate_average(num_list):
     total = 0
     for i in num_list:
         total += i
-    print(f"Average: ", total/length)
-    return
+    return total/length
 
 
 def calculate_min_max(num_list):
-    print(f"Min,Max:", num_list[0], num_list[len(num_list) - 1])
-    return
+    num_list = sort_numlist(num_list)
+    return [num_list[0], num_list[len(num_list) - 1]]
 
 def sort_numlist(num_list):
     return sorted(num_list)
 
 def calculate_median(num_list):
+    num_list = sort_numlist(num_list)
     length = len(num_list)
     if length % 2 == 0:
         #even
         mid = length//2
         median = (num_list[mid - 1] + num_list[mid])/2
     else:
-        mid = length//2 - 1
+        mid = length//2
         median = num_list[mid]
-    print(f"Median: ", median)
-    return
+    return median
 
 def main():
     num_list = get_userinput()
     num_list = sort_numlist(num_list)
-    calculate_average(num_list)
-    calculate_min_max(num_list)
-    calculate_median(num_list)
+    average = calculate_average(num_list)
+    print(f"Average:", average)
+    min_max = calculate_min_max(num_list)
+    print(f"Min,Max:", min_max)
+    median = calculate_median(num_list)
+    print(f"Median:", median)
 
 if __name__ == "__main__":
     main()
